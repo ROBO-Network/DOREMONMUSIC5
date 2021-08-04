23968ab900ee82974e7c81c88fdc911b38b1aa2c
Robo <79803241+ROBO-Network@users.noreply.github.com>
2021-08-04 18:28:25 +0530

    Update start.ts

--- src/handlers/start.ts
+++ src/handlers/start.ts
@@ -3,8 +3,8 @@ import { Composer, Markup } from 'telegraf';
 export const Start = Composer.command('start', async (ctx) => {
     let text =
         `Hi <a href="tg://user?id=${ctx.from.id}">${ctx.from.first_name} ${ctx.from.last_name}</a>\n` +
-        `I Play Songs in Group Voice Chats.\n` +
-        `If you like this bot consider adding a star to the <a href="https://github.com/ArnabXD/TGVCBot">repository</a>`
+        `I Play Songs in Group Voice Chats add me and my assistant @Nobiee_Plugin in your group and ROCK ON🎧.\n` +
+        `Deployed By <a href="https://t.me/itz_aastha_heartlessgirl">❣️Doremon❣️</a>`
     await ctx.replyWithHTML(text, {
         ...Markup.inlineKeyboard([
             [Markup.button.url('View Source', 'https://github.com/ArnabXD/TGVCBot')]
